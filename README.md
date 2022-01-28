25592ee392ab152c5a00dfa5e67d975b25e6f241
Biswajit-97 <jeetmondal42@gmail.com>
2022-01-28 20:57:22 +0530

    trello client ready

---
+++ README.md
@@ -0,0 +1,34 @@
+# Full Stack Trello Clone
+
+# Features
+
+- Fully responsive design like Trello
+- Register and Login
+- Create multiple boards, select any background and add members
+- Create multiple lists
+- Create multiple cards
+- Drag and drop lists or cards
+- Change title and background of board
+- Add description for board
+- Change title of lists
+- Add description, cover color, members, labels, start date, due date, multiple checklist and multiple attachements
+- Delete boards, lists or cards
+- Track all activity logs of cards and boards
+- Comment on cards or boards
+- Search boards or cards titles
+
+# Used Technologies
+
+&nbsp;
+
+## How to run?
+
+- To run it locally just
+  --npm install
+  on project directory
+- Then
+  --npm start
+  the project will start on port 3001
+
+** if wanted to run with backend mounted on local machine paste git clone https://github.com/Biswajit-97/Trello-clone-api.git and run both of them together
+** else if wanted to run only client side, change the baseurl path in services section with hosted url - https://thello---trello-clone.herokuapp.com
---
+++ src/Components/Pages/LoginPage/Login.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { login } from "../../../Services/userService";
+import Background from "../../Background";
+import {
+  BgContainer,
+  Container,
+  TrelloIconContainer,
+  FormSection,
+  FormCard,
+  Form,
+  Title,
+  Input,
+  Button,
+  Icon,
+  Hr,
+  Link,
+} from "./Styled";
+
+const Login = () => {
+  let history = useHistory();
+  const dispatch = useDispatch();
+  const [userInformations, setUserInformations] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(userInformations, dispatch);
+  };
+  return (
+    <>
+      <BgContainer>
+        <Background />
+      </BgContainer>
+      <Container>
+        <TrelloIconContainer onClick={() => history.push("/")}>
+          <Icon src="https://res.cloudinary.com/ddnzqpnvg/image/upload/v1642790059/Logo_ddezeb.svg" />
+        </TrelloIconContainer>
+        <FormSection>
+          <FormCard>
+            <Form onSubmit={(e) => handleSubmit(e)}>
+              <Title>Log in to Thullo</Title>
+              <Input
+                type="email"
+                placeholder="Enter email"
+                required
+                value={userInformations.email}
+                onChange={(e) =>
+                  setUserInformations({
+                    ...userInformations,
+                    email: e.target.value,
+                  })
+                }
+              />
+              <Input
+                type="password"
+                placeholder="Enter password"
+                required
+                value={userInformations.password}
+                onChange={(e) =>
+                  setUserInformations({
+                    ...userInformations,
+                    password: e.target.value,
+                  })
+                }
+              />
+              <Button>Log in</Button>
+              <Hr />
+              <Link
+                fontSize="0.85rem"
+                onClick={() => history.push("/register")}
+              >
+                Sign up for an account
+              </Link>
+            </Form>
+          </FormCard>
+        </FormSection>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
